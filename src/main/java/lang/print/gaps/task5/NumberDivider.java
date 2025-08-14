fadba033fe1b6ca21aa5f5f2540d440c716eb93e
Kirill <cryt.mem@gmail.com>
2025-08-14 11:34:35 +0300

    sol

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -3,5 +3,6 @@ package lang.print.gaps.task5;
 public class NumberDivider {
     void divide(int toBeDivided) {
         double Divided = toBeDivided/5;
+        System.out.println(Divided);
     }
 }
